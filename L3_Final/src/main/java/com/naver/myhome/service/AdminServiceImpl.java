d948c9f503fbc0666b48ef707f57d4ec226c1a01
hyewon <kimbs2305@gmail.com>
2023-10-19 19:01:11 +0900

    conflict 해결

--- L3_Final/src/main/java/com/naver/myhome/service/AdminServiceImpl.java
+++ L3_Final/src/main/java/com/naver/myhome/service/AdminServiceImpl.java
@@ -1,15 +1,18 @@
 package com.naver.myhome.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-
 import com.naver.myhome.domain.Access;
 import com.naver.myhome.domain.Company;
 import com.naver.myhome.domain.Employee;
+import com.naver.myhome.domain.User;
 import com.naver.myhome.mybatis.mapper.AdminMapper;
+import com.naver.myhome.mybatis.mapper.UserMapper;
 
 @Service
 public class AdminServiceImpl implements AdminService {
@@ -94,64 +97,11 @@ public class AdminServiceImpl implements AdminService {
    @Override
    public int changePwd(Map<String, Object> paramMap) {
 	
-
-	@Override
-	public List<Employee> findEmployee(int companyId){
-		return dao.findEmployee(companyId);
-	}
-
-	@Override
-	public int countEmployee(int companyId) {
-		return dao.countEmployee(companyId);
-	}
-
-	@Override
-	public List<Employee> stopEmployee(int companyId) {
-		return dao.stopEmployee(companyId);
-	}
-
-	@Override
-	public int countStopEmployee(int companyId) {
-		return dao.countStopEmployee(companyId);
-	}
-
-	@Override
-	public int stopEmployeeStatus(int employeeId) {
-		return dao.stopEmployeeStatus(employeeId);
-	}
-
-	@Override
-	public int noMoreAuth(int employeeId) {
-		return dao.noMoreAuth(employeeId);
-	}
-
-	@Override
-	public int addAuth(int employeeId) {
-		return dao.addAuth(employeeId);
-	}
-
-	@Override
-	public int useEmployeeStatus(int employeeId) {
-		return dao.useEmployeeStatus(employeeId);
-	}
-
-	@Override
-	public int addEmployee(int userId, int companyId) {
-				return dao.addEmployee(userId, companyId);
-	}
-
-	//회사정보 수정
-	@Override
-	public int updateName(int companyId,String companyName) {
-		return dao.updateName(companyId, companyName);
-	}
-
-	@Override
-	public int updateDomain(int companyId, String companyDomain) {
-		return dao.updateDomain(companyId, companyDomain);
-	}
-
-	@Override
+	   return 0;
+   }
+   
+   
+   @Override
 	public List<Access> getAccesslist() {
 		// TODO Auto-generated method stub
 		return dao.getAccesslist();
@@ -171,21 +121,15 @@ public class AdminServiceImpl implements AdminService {
 
 		
 
-
-}
-
-	   return 0;
-   }
-   
-   
-   @Override
-   public List<Access> getAccesslist() {
-      // TODO Auto-generated method stub
-      return dao.getAccesslist();
-   }
-
       
 
 
 }
+
+	
+  
+      
+
+
+
 
