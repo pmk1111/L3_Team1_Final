64913a967ad749c2e122e25525526761e4ab2991
pmk1111 <kingpmk104@naver.com>
2023-10-22 20:03:00 +0900

    캘린더 일정 CRUD 서버 java 코드 및 mapper xml 제작

--- L3_Final/src/main/java/com/naver/myhome/controller/MyDashboardController.java
+++ L3_Final/src/main/java/com/naver/myhome/controller/MyDashboardController.java
@@ -1,6 +1,8 @@
 package com.naver.myhome.controller;
 
 import java.security.Principal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,86 +20,112 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.naver.myhome.domain.Issue;
 import com.naver.myhome.domain.Memo;
+import com.naver.myhome.domain.Schedule;
 import com.naver.myhome.service.IssueService;
 import com.naver.myhome.service.MemoService;
+import com.naver.myhome.service.ScheduleService;
+import com.naver.myhome.service.UserService;
 
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
 @Controller
 @RequestMapping("/mainboard")
 public class MyDashboardController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(MyDashboardController.class);
-	
+
+	private UserService userService;
 	private IssueService issueService;
 	private MemoService memoService;
-	
+	private ScheduleService scheduleService;
+
 	static int userId = 1;
-	
+
 	@Autowired
-	public MyDashboardController(IssueService issueService, MemoService memoService) {
+	public MyDashboardController(UserService userService, IssueService issueService, 
+			MemoService memoService, ScheduleService scheduleService) {
+		this.userService = userService;
 		this.issueService = issueService;
 		this.memoService = memoService;
+		this.scheduleService = scheduleService;
 	}
-	
+
 	@GetMapping("/my-dashboard")
 	public ModelAndView myDashBoard(ModelAndView mv, HttpServletRequest request, Principal principal) {
-		
-//		시큐리티 적용 전 세션에서 id 가져오기
-//		HttpSession session = request.getSession();
-//		String id = session.getId();
-		
-//		시큐리티 적용 후 세션에서 id 가져오기
-//		String id = principal.getName();
+
+		//		시큐리티 적용 전 세션에서 id 가져오기
+		//		HttpSession session = request.getSession();
+		//		String id = session.getId();
+
+		//		시큐리티 적용 후 세션에서 id 가져오기
+		//		String userEmail = principal.getName();
+		//		int userId = userService.getUserId(userEmail);
 		//나중에는 저 int는 세션에 저장된 사용자 식별번호
-		
+
+		int userId = 1;
+
+		LocalDate currentDate = LocalDate.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		String formattedDate = currentDate.format(formatter); // 오늘날짜 포맷팅한 값
+
 		String toDo = "To Do";
-		List<Issue> mywork = issueService.getMyWork(toDo);
-		List<Memo> memolist = memoService.getMemoContent(userId);
-		
+		List<Issue> myWork = issueService.getMyWork(toDo);
+		List<Memo> memoList = memoService.getMemoContent(userId);
+		List<Schedule> scheduleList = scheduleService.getSelectedScheduleList(formattedDate, userId);
+
 		mv.setViewName("mainboard/my-dashboard");
-		mv.addObject("memolist", memolist);
-		mv.addObject("issuelist", mywork);		
+		mv.addObject("memolist", memoList);
+		mv.addObject("issuelist", myWork);	
+		mv.addObject("schedulelist", scheduleList);
 		return mv;
 	}
-	
-	
-	
+
+	@GetMapping("/get-selected-schedule-list")
+	@ResponseBody
+	public List<Schedule> getSelectedScheduleList(Principal principal, @RequestParam("pickedDate") String pickedDate){
+
+		//		String userEmail = principal.getName();
+		//		int userId = userService.getUserId(userEmail);
+
+		int userId = 1;
+		List<Schedule> scheduleList = scheduleService.getSelectedScheduleList(pickedDate, userId);
+
+		return scheduleList;
+	}
+
 	@GetMapping("/CountPerStatus")
 	@ResponseBody
 	public List<Issue> CountPerStatus(){
 		List<Issue> issuecount = issueService.getStatusCount();
-		
-	    for (Issue issue : issuecount) {
-	        System.out.println("Total Count: " + issue.getTotalcount());
-	        System.out.println("To Do Count: " + issue.getTodocount());
-	        System.out.println("Progress Count: " + issue.getProgresscount());
-	        System.out.println("Resolved Count: " + issue.getResolvedcount());
-	        System.out.println("Done Count: " + issue.getDonecount());
-	    }
-		
+
+		for (Issue issue : issuecount) {
+			System.out.println("Total Count: " + issue.getTotalcount());
+			System.out.println("To Do Count: " + issue.getTodocount());
+			System.out.println("Progress Count: " + issue.getProgresscount());
+			System.out.println("Resolved Count: " + issue.getResolvedcount());
+			System.out.println("Done Count: " + issue.getDonecount());
+		}
+
 		return issuecount;
 	}
-	
+
 	@GetMapping("/mywork")
 	@ResponseBody
 	public List<Issue> getMyWork(@RequestParam String status){
 		List<Issue> mywork = issueService.getMyWork(status);
-		
+
 		return mywork;
 	}
-	
+
 	@GetMapping("/saveMemoContent")
 	@ResponseBody
 	public int saveMemoContent(Memo memo, @RequestParam("memoTxt") String memoTxt) {
 		List<Memo> memoList = memoService.getMemoContent(userId);
 		String currentMemo = memo.getContent();
-		
+
 		int response;
-		
+
 		try {
 			if((memoTxt != "" && memoTxt != null) && memoList.size()==0) {
 				memoService.memoInsert(memoTxt, userId);
@@ -106,14 +134,14 @@ public class MyDashboardController {
 			} else if((memoTxt.trim() == "" || memoTxt == null)  && memoList.size()!=0) {
 				memoService.memoDelete(userId);
 			}
-			
+
 			response = 1;
 		} catch (Exception e) {
 			e.printStackTrace();
 			response = 0;
 		}
-		
+
 		return response;
 	}
-	
+
 }
--- L3_Final/src/main/java/com/naver/myhome/service/ScheduleServiceImpl.java
+++ L3_Final/src/main/java/com/naver/myhome/service/ScheduleServiceImpl.java
@@ -1,11 +1,13 @@
 package com.naver.myhome.service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.naver.myhome.domain.Schedule;
 import com.naver.myhome.mybatis.mapper.ScheduleMapper;
 
 @Service
@@ -18,23 +20,35 @@ public class ScheduleServiceImpl implements ScheduleService{
 		this.mapper = mapper;
 	}
 	
+	@Override
+	public List<Schedule> getScheduleList(int userId) {
+		return mapper.getScheduleList(userId);
+	}
 
 	@Override
-	public void insertSchedule(Map<String, Object> map) {
-		Map<String, String> paramMap = new HashMap<String, String>();
-//		 
-		paramMap.put("id", (String)map.get("id"));
-		paramMap.put("user_id", (String)map.get("user_id"));
-		paramMap.put("subject", (String)map.get("subject"));
-		paramMap.put("start_date", (String)map.get("start_date"));
-		paramMap.put("end_date", (String)map.get("end_date"));
-		paramMap.put("category", (String)map.get("category"));
-		paramMap.put("place", (String)map.get("place"));
-		paramMap.put("content", (String)map.get("content"));
-		 
-		mapper.insertSchedule(paramMap);
+	public void insertSchedule(Schedule schedule) {
+		mapper.insertSchedule(schedule);
 		
 	}
+
+	@Override
+	public Schedule getScheduleInfo(int scheduleId) {
+		return mapper.getScheduleInfo(scheduleId);
+	}
 	
+	@Override
+	public int updateSchedule(Schedule schedule) {
+		return mapper.updateSchedule(schedule);
+	}
+
+	@Override
+	public int deleteSchedule(int scheduleId) {
+		return mapper.deleteSchedule(scheduleId);
+	}
+
+	@Override
+	public List<Schedule> getSelectedScheduleList(String pickedDate, int userId) {
+		return mapper.getSelectedScheduleList(pickedDate, userId);
+	}
 	
 }
