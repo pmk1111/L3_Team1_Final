4230824b608d84634d77c0f58b2f51b3692d75b9
pmk1111 <kingpmk104@naver.com>
2023-11-21 21:08:07 +0900

    불필요한 주석 제거 및 로그인 인터셉터 추가

--- L3_Final/src/main/java/com/naver/myhome/WebMvcConfig.java
+++ L3_Final/src/main/java/com/naver/myhome/WebMvcConfig.java
@@ -2,34 +2,55 @@ package com.naver.myhome;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.naver.myhome.interceptor.AuthenticationInterceptor;
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
-	
+
 	@Value("${my.savepath}")
-	   private String saveFolder;
+	private String saveFolder;
 
 	private static final String[] CLASSPATH_RESOURCE_LOCATIONS
 	= {
 			"classpath:/static/",
 			"classpath:/templates/",
 	};
-	
+
 	@Override
 	public void addViewControllers(ViewControllerRegistry registry) {
 		registry.addViewController("/").setViewName("forward:/home/home");
 	}
-	
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**")
 		.addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
-		   registry.addResourceHandler("/upload/**")
-		     .addResourceLocations( saveFolder  ); 
-		     
+		registry.addResourceHandler("/upload/**")
+		.addResourceLocations( saveFolder  ); 
+
 	}
-	
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+	    String[] excludePatterns = {
+	    	"/widus/",
+	        "/user/login",
+	        "/user/join",
+	        "/user/check-email",
+	        "/user/send-mail-auth-code",
+	        "/user/check-auth-code",
+	        "/resources/**"
+	    };
+
+	    registry.addInterceptor(new AuthenticationInterceptor())
+	            .addPathPatterns("/**")
+	            .excludePathPatterns(excludePatterns);
+	}
+
+
 }
